149e781b7d8db8b79c98dc117bf6c5bd2a01ca3e
Damian Zimon <zimondamian@gmail.com>
2022-06-06 12:15:12 +0200

    Functional mode: context for directed graphs

--- src/functional.cc
+++ src/functional.cc
@@ -25,6 +25,7 @@
 #include "graphgenerator.hpp"
 #include "graphreader.hpp"
 #include "mst.hpp"
+#include "shortestpath.hpp"
 #include "test.hpp"
 
 namespace sdizo::test {
@@ -105,6 +106,110 @@ class Ctx {
       else
         std::printf("?%s\n", pair.first.c_str());
   }
+};
+
+class Directed : public Ctx {
+ public:
+  Directed() {
+    cmds_["generate"] = std::make_pair("<vertices> <density>", std::bind(&Directed::GenerateGraph, this, _1));
+    cmds_["list"] = std::make_pair("", std::bind(&Directed::PrintList, this, _1));
+    cmds_["matrix"] = std::make_pair("", std::bind(&Directed::PrintMatrix, this, _1));
+    cmds_["dijkstra"] = std::make_pair("{list | matrix}", std::bind(&Directed::Dijkstra, this, _1));
+    cmds_["bellmanford"] = std::make_pair("{list | matrix}", std::bind(&Directed::BellmanFord, this, _1));
+  }
+
+  const char* Name() const { return "directed"; }
+
+  void Load(const std::vector<WEdge>& edges, const size_t vertices, const Vertex vb) {
+    vb_ = vb;
+    g_matrix_ = std::make_shared<AdjacencyMatrix>(true, vertices);
+    g_list_ = std::make_shared<AdjacencyList>(true, vertices);
+    std::for_each(edges.cbegin(), edges.cend(), [this](const WEdge& edge) {
+      g_list_->AddEdge(edge);
+      g_matrix_->AddEdge(edge);
+    });
+  }
+
+ private:
+  void PrintList(std::string_view) const {
+    if (g_list_ != nullptr)
+      g_list_->Print();
+    else
+      std::printf("Error: Adjacency list is empty\n");
+  }
+  void PrintMatrix(std::string_view) const {
+    if (g_matrix_ != nullptr)
+      g_matrix_->Print();
+    else
+      std::printf("Error: Adjacency matrix is empty\n");
+  }
+  void Dijkstra(std::string_view line) const {
+    if (g_list_ == nullptr || g_matrix_ == nullptr) {
+      std::printf("Error: Graph does not exist\n");
+      return;
+    }
+    std::string_view token;
+    if (!GetToken(line, token)) {
+      std::printf("Error: Missing argument\n");
+      return;
+    }
+    if (token.compare("list"sv) == 0)
+      detail::Print(vb_, *shortestpath::Dijkstra<AdjacencyList>(g_list_, vb_));
+    else if (token.compare("matrix"sv) == 0)
+      detail::Print(vb_, *shortestpath::Dijkstra<AdjacencyMatrix>(g_matrix_, vb_));
+    else
+      std::printf("Error: Invalid graph representaton\n");
+  }
+  void BellmanFord(std::string_view line) const {
+    if (g_list_ == nullptr || g_matrix_ == nullptr) {
+      std::printf("Error: Graph does not exist\n");
+      return;
+    }
+    std::string_view token;
+    if (!GetToken(line, token)) {
+      std::printf("Error: Missing argument\n");
+      return;
+    }
+    std::unique_ptr<PathCost> path_cost = std::make_unique<PathCost>();
+    if (token.compare("list"sv) == 0)
+      path_cost = shortestpath::BellmanFord<AdjacencyList>(g_list_, vb_);
+    else if (token.compare("matrix"sv) == 0)
+      path_cost = shortestpath::BellmanFord<AdjacencyMatrix>(g_matrix_, vb_);
+    else {
+      std::printf("Error: Invalid graph representaton\n");
+      return;
+    }
+    if (path_cost)
+      detail::Print(vb_, *path_cost);
+    else
+      std::printf("Warning: Detected negative cycle\n");
+  }
+
+  void GenerateGraph(std::string_view line) {
+    std::string_view token;
+    if (!GetToken(line, token, "vertices")) return;
+    long vertices;
+    if (!ParseNum(token, vertices, "vertices")) return;
+    if (vertices < 1) {
+      std::printf("Error: Invalid number of vertices, should be greater than 0\n");
+      return;
+    }
+    if (!GetToken(line, token, "density")) return;
+    int density;
+    if (!ParseNum(token, density, "density")) return;
+    if (density < 1 || density > 100) {
+      std::printf("Error: Invalid density, should be 0 < density <= 100\n");
+      return;
+    }
+    Vertex vb;
+    std::vector<WEdge> edges = graph_gen_.Generate(vertices, density, true, &vb);
+    Load(edges, vertices, vb);
+  }
+
+  std::shared_ptr<AdjacencyList> g_list_{nullptr};
+  std::shared_ptr<AdjacencyMatrix> g_matrix_{nullptr};
+  GraphGenerator graph_gen_{true};
+  Vertex vb_;
 };
 
 class Undirected : public Ctx {
@@ -204,12 +309,31 @@ class Undirected : public Ctx {
 class Main : public Ctx {
  public:
   Main(CtxPtr* ctx, const char* input) : input_(input), ctx_ref_(ctx) {
+    cmds_["directed"] = std::make_pair("[init]", std::bind(&Main::EnterDirected, this, _1));
     cmds_["undirected"] = std::make_pair("[init]", std::bind(&Main::EnterUndirected, this, _1));
   }
 
   const char* Name() const override { return ""; }
 
  private:
+  void EnterDirected(std::string_view line) {
+    std::shared_ptr<Directed> ctx_undirected = std::make_shared<Directed>();
+    *ctx_ref_ = ctx_undirected;
+    std::string_view token;
+    if (!GetToken(line, token) || token.compare("init"sv) != 0) return;
+    if (input_ == nullptr) {
+      std::printf("Error: No --input provided\n");
+      return;
+    }
+    std::vector<WEdge> edges;
+    size_t vertices;
+    Vertex vb;
+    if (!LoadGraph(edges, vertices, input_, &vb)) {
+      std::printf("Error: Loading graph\n");
+      return;
+    }
+    ctx_undirected->Load(edges, vertices, vb);
+  }
   void EnterUndirected(std::string_view line) {
     std::shared_ptr<Undirected> ctx_undirected = std::make_shared<Undirected>();
     *ctx_ref_ = ctx_undirected;
